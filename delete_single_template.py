858339e09094bcb98499d7c33f8e5c912116ce29
Deadman <jdhanspal@gmail.com>
2020-05-28 09:33:47 -0400

    made changes
    made changes

--- delete_single_template.py
+++ delete_single_template.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import requests
 import sys
 
@@ -17,8 +16,8 @@ sess = requests.session()
 response = sess.post(url=url, data=data, verify=False)
 
 if response.status_code !=  200:
-    print('Login Failed')
-    sys.exit(0)
+	print('Login Failed')
+	sys.exit(0)
 
 
 url = baseurl +'/dataservice/template/feature'
@@ -26,24 +25,31 @@ url = baseurl +'/dataservice/template/feature'
 response = sess.get(url=url, verify=False)
 listids = []
 data = response.json()
+
+
 for x in data['data']:
     if x['factoryDefault'] != True:
-
         listids.append(x['templateId'])
+
 if listids == []:
     print('There are no custom feature Templates to delete')
     sys.exit('No Templates')
 
-ans = input('Do you want to delete ALL custom feature Templates? Y or N: ')
-if ans.lower() == 'y':
-    print('The following templates will be deleted\n')
-    for x in data['data']:
-        if x['factoryDefault'] != True:
-            print(x['templateName'])
-    newans = input('\n Again, do you really want to delete all these feature Templates? Y or N]: ')
-    if newans.lower() == 'y':
-        for x in listids:
-            response = sess.delete(url=url + '/' + x)
-            print(response.status_code)
 
+print('Which template do you want to delete\n')
+for x in data['data']:
+    if x['factoryDefault'] != True:
+        print(x['templateName'])
+
+
+newans = input('\nEnter name here:  ')
+
+for x in data['data']:
+   if x['factoryDefault'] != True:
+        if newans.lower() == x['templateName'].lower():
+            response = sess.delete(url=url + '/' + x['templateId'])
+            print("Status code: " + str(response.status_code) + f" ...  Template {newans} deleted successfully")
+            break
+else:
+    print("Error occured, Entered name not found..")
 
